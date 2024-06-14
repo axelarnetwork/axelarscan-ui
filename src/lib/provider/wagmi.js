a227f05604480b61d670d97d29e696e4a1ea7bea
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-06-14 02:36:59 -0400

    chore: update to use chain config from wagmi/chains

--- src/lib/provider/wagmi.js
+++ src/lib/provider/wagmi.js
@@ -1,6 +1,6 @@
 import { QueryClient } from '@tanstack/react-query'
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-import { mainnet, goerli, sepolia, bsc, bscTestnet, polygon, polygonMumbai, polygonZkEvm, polygonZkEvmTestnet, avalanche, avalancheFuji, fantom, fantomTestnet, moonbeam, moonbaseAlpha, aurora, auroraTestnet, arbitrum, arbitrumGoerli, arbitrumSepolia, optimism, optimismGoerli, optimismSepolia, base, baseGoerli, baseSepolia, mantle, mantleTestnet, celo, celoAlfajores, kava, kavaTestnet, filecoin, filecoinHyperspace, filecoinCalibration, linea, lineaTestnet, scroll, scrollSepolia } from 'wagmi/chains'
+import { mainnet, goerli, sepolia, bsc, bscTestnet, polygon, polygonMumbai, polygonAmoy, polygonZkEvm, polygonZkEvmTestnet, avalanche, avalancheFuji, fantom, fantomTestnet, moonbeam, moonbaseAlpha, aurora, auroraTestnet, arbitrum, arbitrumGoerli, arbitrumSepolia, optimism, optimismGoerli, optimismSepolia, base, baseGoerli, baseSepolia, mantle, mantleTestnet, mantleSepoliaTestnet, celo, celoAlfajores, kava, kavaTestnet, filecoin, filecoinHyperspace, filecoinCalibration, linea, lineaTestnet, lineaSepolia, scroll, scrollSepolia, immutableZkEvm, immutableZkEvmTestnet, fraxtal, fraxtalTestnet, blast, blastSepolia } from 'wagmi/chains'
 
 export const CHAINS = process.env.NEXT_PUBLIC_ENVIRONMENT === 'mainnet' ?
   [
@@ -22,16 +22,16 @@ export const CHAINS = process.env.NEXT_PUBLIC_ENVIRONMENT === 'mainnet' ?
     { _id: 'linea', ...linea },
     { _id: 'centrifuge', id: 2031, network: 'centrifuge', name: 'Centrifuge', nativeCurrency: { name: 'Centrifuge', symbol: 'CFG', decimals: 18 }, rpcUrls: { default: { http: ['https://fullnode.parachain.centrifuge.io'] }, public: { http: ['https://fullnode.parachain.centrifuge.io'] } }, blockExplorers: { default: { name: 'Centrifuge', url: 'https://centrifuge.subscan.io' } } },
     { _id: 'scroll', ...scroll },
-    { _id: 'immutable', id: 13371, network: 'immutable', name: 'Immutable', nativeCurrency: { name: 'ImmutableX', symbol: 'IMX', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.immutable.com'] }, public: { http: ['https://rpc.immutable.com'] } }, blockExplorers: { default: { name: 'Immutable', url: 'https://explorer.immutable.com' } } },
-    { _id: 'fraxtal', id: 252, network: 'fraxtal', name: 'Fraxtal', nativeCurrency: { name: 'Frax Ether', symbol: 'frxETH', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.frax.com'] }, public: { http: ['https://rpc.frax.com'] } }, blockExplorers: { default: { name: 'Fraxtal', url: 'https://fraxscan.com' } } },
-    { _id: 'blast', id: 81457, network: 'blast', name: 'Blast', nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.ankr.com/blast'] }, public: { http: ['https://rpc.ankr.com/blast'] } }, blockExplorers: { default: { name: 'Blast', url: 'https://blastscan.io' } } },
+    { _id: 'immutable', ...immutableZkEvm },
+    { _id: 'fraxtal', ...fraxtal },
+    { _id: 'blast', ...blast },
   ] :
   [
     { _id: 'ethereum-sepolia', ...sepolia },
     // { _id: 'ethereum-2', ...goerli },
     { _id: 'binance', ...bscTestnet },
     // { _id: 'polygon', ...polygonMumbai },
-    { _id: 'polygon-sepolia', id: 80002, network: 'polygon', name: 'Polygon Sepolia', nativeCurrency: { name: 'Polygon', symbol: 'MATIC', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc-amoy.polygon.technology'] }, public: { http: ['https://rpc-amoy.polygon.technology'] } }, blockExplorers: { default: { name: 'Polygon', url: 'https://amoy.polygonscan.com' } } },
+    { _id: 'polygon-sepolia', ...polygonAmoy },
     { _id: 'polygon-zkevm', ...polygonZkEvmTestnet },
     { _id: 'avalanche', ...avalancheFuji },
     { _id: 'fantom', ...fantomTestnet },
@@ -44,20 +44,20 @@ export const CHAINS = process.env.NEXT_PUBLIC_ENVIRONMENT === 'mainnet' ?
     // { _id: 'base', ...baseGoerli },
     { _id: 'base-sepolia', ...baseSepolia },
     // { _id: 'mantle', ...mantleTestnet },
-    { _id: 'mantle-sepolia', id: 5003, network: 'mantle', name: 'Mantle Sepolia', nativeCurrency: { name: 'Mantle', symbol: 'MNT', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.sepolia.mantle.xyz'] }, public: { http: ['https://rpc.sepolia.mantle.xyz'] } }, blockExplorers: { default: { name: 'Mantle', url: 'https://explorer.sepolia.mantle.xyz' } } },
+    { _id: 'mantle-sepolia', ...mantleSepoliaTestnet },
     { _id: 'celo', ...celoAlfajores },
     { _id: 'kava', ...kavaTestnet },
     // { _id: 'filecoin', ...filecoinHyperspace },
     { _id: 'filecoin-2', ...filecoinCalibration },
     // { _id: 'linea', ...lineaTestnet },
-    { _id: 'linea-sepolia', id: 59141, network: 'linea-sepolia', name: 'Linea Sepolia', nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.sepolia.linea.build'] }, public: { http: ['https://rpc.sepolia.linea.build'] } }, blockExplorers: { default: { name: 'Linea', url: 'https://sepolia.lineascan.build' } } },
+    { _id: 'linea-sepolia', ...lineaSepolia },
     // { _id: 'centrifuge', id: 2089, network: 'centrifuge', name: 'Centrifuge', nativeCurrency: { name: 'Algol', symbol: 'ALGL', decimals: 18 }, rpcUrls: { default: { http: ['https://fullnode.algol.cntrfg.com/rpc'] }, public: { http: ['https://fullnode.algol.cntrfg.com/rpc'] } }, blockExplorers: { default: { name: 'Centrifuge', url: '' } } },
     { _id: 'centrifuge-2', id: 2090, network: 'centrifuge', name: 'Centrifuge', nativeCurrency: { name: 'Algol', symbol: 'ALGL', decimals: 18 }, rpcUrls: { default: { http: ['https://fullnode.demo.k-f.dev'] }, public: { http: ['https://fullnode.demo.k-f.dev'] } }, blockExplorers: { default: { name: 'Centrifuge', url: '' } } },
     { _id: 'scroll', ...scrollSepolia },
-    { _id: 'immutable', id: 13473, network: 'immutable', name: 'Immutable', nativeCurrency: { name: 'ImmutableX', symbol: 'tIMX', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.testnet.immutable.com'] }, public: { http: ['https://rpc.testnet.immutable.com'] } }, blockExplorers: { default: { name: 'Immutable', url: 'https://explorer.testnet.immutable.com' } } },
+    { _id: 'immutable', ...immutableZkEvmTestnet },
     { _id: 'immutable-devnet', id: 15003, network: 'immutable', name: 'Immutable', nativeCurrency: { name: 'ImmutableX', symbol: 'tIMX', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.dev.immutable.com'] }, public: { http: ['https://rpc.dev.immutable.com'] } }, blockExplorers: { default: { name: 'Immutable', url: 'https://explorer.testnet.immutable.com' } } },
-    { _id: 'fraxtal', id: 2522, network: 'fraxtal', name: 'Fraxtal', nativeCurrency: { name: 'Frax Ether', symbol: 'frxETH', decimals: 18 }, rpcUrls: { default: { http: ['https://rpc.testnet.frax.com'] }, public: { http: ['https://rpc.testnet.frax.com'] } }, blockExplorers: { default: { name: 'Fraxtal', url: 'https://holesky.fraxscan.com' } } },
-    { _id: 'blast-sepolia', id: 168587773, network: 'blast', name: 'Blast', nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 }, rpcUrls: { default: { http: ['https://sepolia.blast.io'] }, public: { http: ['https://sepolia.blast.io'] } }, blockExplorers: { default: { name: 'Blast', url: 'https://testnet.blastscan.io' } } },
+    { _id: 'fraxtal', ...fraxtalTestnet },
+    { _id: 'blast-sepolia', ...blastSepolia },
   ]
 
 export const queryClient = new QueryClient()
