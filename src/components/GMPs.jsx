3c718dd603014e9ac9e44bce5bb848c94cdccce5
pjenviri <87146398+pjenviri@users.noreply.github.com>
2024-11-13 10:50:45 +0700

    fix typo

--- src/components/GMPs.jsx
+++ src/components/GMPs.jsx
@@ -318,7 +318,7 @@ export function GMPs({ address }) {
         <div>
           <div className="flex items-center justify-between gap-x-4">
             <div className="sm:flex-auto">
-              <h1 className="text-zinc-900 dark:text-zinc-100 text-base font-semibold leading-6">General Message Passings</h1>
+              <h1 className="text-zinc-900 dark:text-zinc-100 text-base font-semibold leading-6">General Message Passing</h1>
               <p className="mt-2 text-zinc-400 dark:text-zinc-500 text-sm">
                 <Number value={total} suffix={` result${total > 1 ? 's' : ''}`} /> 
               </p>
