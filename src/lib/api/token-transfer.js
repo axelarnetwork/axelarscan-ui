314429c911c7d59c561ec697e96527970ce8e334
Sirapop <n.r.sirapop@gmail.com>
2025-02-20 23:19:49 +0900

    chore: handle param object

--- src/lib/api/token-transfer.js
+++ src/lib/api/token-transfer.js
@@ -1,6 +1,7 @@
 import { objToQS } from '@/lib/parser'
 
 const request = async (method, params, requestMethod = 'GET') => {
+  requestMethod = Object.values({ ...params }).findIndex(v => v && typeof v === 'object') > -1 ? 'POST' : requestMethod
   const response = await fetch(`${process.env.NEXT_PUBLIC_TOKEN_TRANSFER_API_URL}/${method}${requestMethod === 'GET' ? objToQS(params) : ''}`, { method: requestMethod, body: requestMethod === 'GET' ? undefined : JSON.stringify(params) }).catch(error => { return null })
   return response && await response.json()
 }
