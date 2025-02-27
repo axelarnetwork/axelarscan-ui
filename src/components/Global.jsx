98308ebc5cf7aeb8b37b47cafd8256e352dd03b3
Sirapop <n.r.sirapop@gmail.com>
2025-02-28 08:19:26 +0900

    chore: filter vm chains

--- src/components/Global.jsx
+++ src/components/Global.jsx
@@ -46,7 +46,7 @@ export function Global() {
       await Promise.all(['chains', 'assets', 'itsAssets', 'contracts', 'configurations', 'validators', 'verifiers', 'inflationData', 'networkParameters', 'tvl', 'stats'].map(k => new Promise(async resolve => {
         switch (k) {
           case 'chains':
-            setChains(await getChains())
+            setChains(toArray(await getChains()).filter(d => d.chain_type !== 'vm' || d.voting_verifier?.address))
             break
           case 'assets':
             const assets = await getAssets()
