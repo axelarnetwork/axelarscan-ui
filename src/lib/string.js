eb10a35c851674bb1624fedb08cab361843a7943
João Azevedo <joaoazevedo@ieee.org>
2025-08-04 15:02:43 +0200

    fix: fix broken links in amplifier proofs

--- src/lib/string.js
+++ src/lib/string.js
@@ -7,6 +7,7 @@ export const equalsIgnoreCase = (a, b) => (!a && !b) || toCase(a, 'lower') === t
 export const capitalize = string => !isString(string) ? '' : `${string.substr(0, 1).toUpperCase()}${string.substr(1)}`
 export const camel = (string, delimiter = '_') => toArray(string, { delimiter }).map((s, i) => i > 0 ? capitalize(s) : s).join('')
 export const removeDoubleQuote = string => !isString(string) ? string : split(string, { delimiter: '"' }).join('')
+export const removeHexPrefix = (string) => !isString(string) ? string : string.startsWith('0x') ? string.slice(2) : string
 export const toBoolean = (string, defaultValue = true) => typeof string === 'boolean' ? string : !isString(string) ? defaultValue : equalsIgnoreCase(string, 'true')
 export const headString = (string, delimiter = '-') => _.head(split(string, { delimiter }))
 export const lastString = (string, delimiter = '-') => _.last(split(string, { delimiter }))
