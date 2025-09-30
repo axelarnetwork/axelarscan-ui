5526b73ebf01c0ab1dff041c12439eeb9f90aaf8
Nikos Vlastaras <nikosvlast@gmail.com>
2025-09-30 17:17:14 +0300

    fix: build correct tx url for stellar

--- src/components/ExplorerLink.jsx
+++ src/components/ExplorerLink.jsx
@@ -12,6 +12,34 @@ import { getInputType } from '@/lib/parser'
 import { isString } from '@/lib/string'
 import { isNumber } from '@/lib/number'
 
+export const buildExplorerURL = (value, type, useContractLink, hasEventLog, explorer) => {
+  const { url, address_path, contract_path, contract_0_path, transaction_path, block_path, no_0x, cannot_link_contract_via_address_path } = { ...explorer }
+
+  let path
+  let processedValue = value
+
+  switch (type) {
+    case 'address':
+      path = useContractLink && cannot_link_contract_via_address_path && contract_path ? contract_path : address_path
+      return `${url}${path?.replace(`{address}`, processedValue)}`
+    case 'contract':
+      path = (value === ZeroAddress && contract_0_path) || contract_path
+      return `${url}${path?.replace(`{address}`, processedValue)}`
+    case 'tx':
+      path = transaction_path
+      // remove prefix 0x
+      if (no_0x && processedValue?.startsWith('0x')) {
+        processedValue = processedValue.substring(2)
+      }
+      return `${url}${path?.replace(`{tx}`, processedValue)}${value.startsWith('0x') && hasEventLog ? '#eventlog' : ''}`
+    case 'block':
+      path = block_path
+      return `${url}${path?.replace(`{block}`, processedValue)}`
+    default:
+      return undefined
+  }
+}
+
 export function ExplorerLink({
   value,
   chain,
@@ -29,7 +57,6 @@ export function ExplorerLink({
 }) {
   const { chains } = useGlobalStore()
   const { explorer } = { ...getChainData(chain, chains) }
-  const { url, name, block_path, address_path, contract_path, contract_0_path, transaction_path, icon, no_0x, cannot_link_contract_via_address_path } = { ...explorer }
 
   if (type === 'tx') {
     // update type from input value
@@ -47,46 +74,21 @@ export function ExplorerLink({
     }
   }
 
-  let path
-  let field = type
+  let href = customURL || buildExplorerURL(value, type, useContractLink, hasEventLog, explorer)
 
-  // set explorer path by input type
-  switch (type) {
-    case 'address':
-      path = useContractLink && cannot_link_contract_via_address_path && contract_path ? contract_path : address_path
-      break
-    case 'contract':
-      path = (value === ZeroAddress && contract_0_path) || contract_path
-      field = 'address'
-      break
-    case 'tx':
-      path = transaction_path
-
-      // remove prefix 0x
-      if (no_0x && value?.startsWith('0x')) {
-        value = value.substring(2)
-      }
-      break
-    case 'block':
-      path = block_path
-      break
-    default:
-      break
-  }
-
-  return (customURL || (url && value)) && (
+  return href && (
     <Link
-      href={customURL || `${url}${path?.replace(`{${field}}`, value)}${type === 'tx' && value.startsWith('0x') && hasEventLog ? '#eventlog' : ''}`}
+      href={href}
       target="_blank"
       className={clsx('min-w-max flex items-center gap-x-2', containerClassName)}
     >
       {!iconOnly && (
         <span className={clsx('font-medium', nonIconClassName)}>
-          {title || `View on ${name}`}
+          {title || `View on ${explorer.name}`}
         </span>
       )}
       <Image
-        src={icon}
+        src={explorer.icon}
         alt=""
         width={width}
         height={height}
