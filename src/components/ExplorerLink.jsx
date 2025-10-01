3df3401780b8c6a82942bd134ae113c57e9979f4
Nikos Vlastaras <nikosvlast@gmail.com>
2025-10-01 11:40:13 +0300

    address comments

--- src/components/ExplorerLink.jsx
+++ src/components/ExplorerLink.jsx
@@ -15,38 +15,46 @@ import { isNumber } from '@/lib/number'
 export const buildExplorerURL = ({ value, type, useContractLink, hasEventLog, explorer }) => {
   const { url, address_path, contract_path, contract_0_path, transaction_path, block_path, no_0x, cannot_link_contract_via_address_path } = { ...explorer }
 
-  // Return undefined if url or value are falsy
+  // Return a fallback URL if url or value are falsy
   if (!url || !value) {
-    return undefined
+    return '#'
   }
 
   let path
   let href
-  let processedValue = value
+  let processedValue
+  let suffix
 
   switch (type) {
     case 'address':
       path = useContractLink && cannot_link_contract_via_address_path && contract_path ? contract_path : address_path
-      href = `${url}${path?.replace(`{address}`, processedValue)}`
+      href = `${url}${path?.replace(`{address}`, value)}`
       break;
     case 'contract':
       path = (value === ZeroAddress && contract_0_path) || contract_path
-      href = `${url}${path?.replace(`{address}`, processedValue)}`
+      href = `${url}${path?.replace(`{address}`, value)}`
       break;
     case 'tx':
       path = transaction_path
-      // remove prefix 0x
-      if (no_0x && processedValue?.startsWith('0x')) {
-        processedValue = processedValue.substring(2)
+
+      processedValue = value
+      if (no_0x && value?.startsWith('0x')) {
+        processedValue = value.substring(2)
       }
-      href = `${url}${path?.replace(`{tx}`, processedValue)}${processedValue.startsWith('0x') && hasEventLog ? '#eventlog' : ''}`
+
+      suffix = ''
+      if (hasEventLog && value?.startsWith('0x')) {
+        suffix = '#eventlog'
+      }
+
+      href = `${url}${path?.replace(`{tx}`, processedValue)}${suffix}`
       break;
     case 'block':
       path = block_path
-      href = `${url}${path?.replace(`{block}`, processedValue)}`
+      href = `${url}${path?.replace(`{block}`, value)}`
       break;
     default:
-      return undefined
+      return '#'
   }
 
   return href
